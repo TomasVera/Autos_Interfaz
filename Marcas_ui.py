976f1213502b39d77c4e9f2545d9ffd9cf3dafea
Daniel <daniel.inchains@hotmail.com>
2014-07-11 05:23:10 -0400

    1.0

---
+++ Marcas_ui.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Marcas.ui'
+#
+# Created: Fri Jul  4 13:04:35 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Marcas")
+        Dialog.resize(435, 395)
+        self.gridLayout = QtGui.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.btn_nueva_marca = QtGui.QPushButton(Dialog)
+        self.btn_nueva_marca.setObjectName("btn_nueva_marca")
+        self.horizontalLayout.addWidget(self.btn_nueva_marca)
+        self.btn_editar_marca = QtGui.QPushButton(Dialog)
+        self.btn_editar_marca.setObjectName("btn_editar_marca")
+        self.horizontalLayout.addWidget(self.btn_editar_marca)
+        self.btn_eliminar_marca = QtGui.QPushButton(Dialog)
+        self.btn_eliminar_marca.setObjectName("btn_eliminar_marca")
+        self.horizontalLayout.addWidget(self.btn_eliminar_marca)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.marca_table = QtGui.QTableView(Dialog)
+        self.marca_table.setObjectName("marca_table")
+        self.gridLayout.addWidget(self.marca_table, 1, 0, 1, 1)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.horizontalLayout_2.addWidget(self.label)
+        self.abrir_auto2 = QtGui.QPushButton(Dialog)
+        self.abrir_auto2.setObjectName("abrir_auto2")
+        self.horizontalLayout_2.addWidget(self.abrir_auto2)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 2, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Marcas", "Marcas", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_nueva_marca.setText(QtGui.QApplication.translate("Dialog", "Nueva", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_editar_marca.setText(QtGui.QApplication.translate("Dialog", "Editar", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_eliminar_marca.setText(QtGui.QApplication.translate("Dialog", "Eliminar", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Dialog", "Ir a:", None, QtGui.QApplication.UnicodeUTF8))
+        self.abrir_auto2.setText(QtGui.QApplication.translate("Dialog", "Autos", None, QtGui.QApplication.UnicodeUTF8))
+
---
+++ main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import sys
+from PySide import QtGui, QtCore
+from MainWindow import Ui_MainWindow
+import Marcas
+import Autos
+import os
+
+
+class Main(QtGui.QMainWindow):
+
+	tabla_columnas = (
+		(u"Título",200),
+		(u"Año",50),
+		(u"Director",150),
+		(u"País",50),
+		(u"Rank",40),
+		)
+
+	def __init__(self, parent=None):
+		QtGui.QMainWindow.__init__(self, parent)
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.show()
+		self.connect_actions()
+
+	def connect_actions(self):
+		self.ui.abrir_marcas.clicked.connect(self.action_abrir_marcas)
+		self.ui.abrir_autos.clicked.connect(self.action_abrir_autos)
+
+	def action_abrir_marcas(self):
+		self.setVisible(False)
+		self.marcasWindow = Marcas.Marcas()
+		self.marcasWindow.exec_()
+	
+	def action_abrir_autos(self):
+		self.setVisible(False)
+		self.autosWindow = Autos.Autos()
+		self.autosWindow.exec_()
+		
+
+def run():
+	app = QtGui.QApplication(sys.argv)
+	main = Main()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	run()
